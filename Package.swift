df70d7e7a178699c71056f73f1481a8cf77c2be9
Mikhail Vospennikov <mikhail.vospennikov@zing.coach>
2026-04-16 17:16:59 +0300

    Version 1.3.0

--- Package.swift
+++ Package.swift
@@ -9,26 +9,25 @@ let package = Package(
         .library(name: "ZingCoach", targets: ["ZingCoach"])
     ],
     dependencies: [
-        .package(
-            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios-dynamic-dependencies.git",
-            .upToNextMajor(from: "1.0.1")
-        ),
+        .package(url: "https://github.com/Muze-Fitness/zing-coach-ios-tools", exact: "1.0.1"),
+        .package(url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios-dynamic-dependencies.git", exact: "1.0.1"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "4.5.1")),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
     ],
     targets: [
         .binaryTarget(
             name: "ZingCoachSDK",
-            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios/releases/download/1.2.0/ZingCoachSDK.xcframework.zip",
-            checksum: "1e94b210432fe23c1a534b0d112e1dbb568d0a7bd0ec190bc8f0a57808975e1d"
+            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios/releases/download/1.3.0/ZingCoachSDK.xcframework.zip",
+            checksum: "d8667c8ff932437defcbfa0648bef19dbd4cb53a295164c9d46f56ef5fcbeb7f"
         ),
         .target(
             name: "ZingCoach",
             dependencies: [
                 .target(name: "ZingCoachSDK"),
+                .product(name: "DesignSystem", package: "zing-coach-ios-tools"),
+                .product(name: "ZingCoachDynamicDependencies", package: "zing-coach-sdk-ios-dynamic-dependencies"),
                 .product(name: "Lottie-Dynamic", package: "lottie-ios"),
                 .product(name: "SnapKit-Dynamic", package: "SnapKit"),
-                .product(name: "ZingCoachDynamicDependencies", package: "zing-coach-sdk-ios-dynamic-dependencies"),
             ],
             path: "Sources/ZingCoach"
         ),
--- Sources/ZingCoach/ZingCoach.swift
+++ Sources/ZingCoach/ZingCoach.swift
@@ -1,1 +1,2 @@
 @_exported import ZingCoachSDK
+@_exported import DesignSystem
