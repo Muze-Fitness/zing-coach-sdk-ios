4d9eb5a9b0f9815053169bcdf9b1b34380e98f49
Mikhail Vospennikov <mikhail.vospennikov@zing.coach>
2026-05-19 12:05:25 +0300

    Version 1.5.0

--- Package.swift
+++ Package.swift
@@ -10,22 +10,20 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Muze-Fitness/zing-coach-ios-tools", exact: "1.2.0"),
-        .package(url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios-dynamic-dependencies.git", exact: "1.0.1"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "4.5.1")),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
     ],
     targets: [
         .binaryTarget(
             name: "ZingCoachSDK",
-            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios/releases/download/1.4.2/ZingCoachSDK.xcframework.zip",
-            checksum: "03b92d4b96305f88d4e10cf69b046954927f9b053435d0131e0fe7a6df189047"
+            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios/releases/download/1.5.0/ZingCoachSDK.xcframework.zip",
+            checksum: "2655f6557a724810169e427dc399f1d92a726447ddffc853f6b784ea655679b3"
         ),
         .target(
             name: "ZingCoach",
             dependencies: [
                 .target(name: "ZingCoachSDK"),
                 .product(name: "DesignSystem", package: "zing-coach-ios-tools"),
-                .product(name: "ZingCoachDynamicDependencies", package: "zing-coach-sdk-ios-dynamic-dependencies"),
                 .product(name: "Lottie-Dynamic", package: "lottie-ios"),
                 .product(name: "SnapKit-Dynamic", package: "SnapKit"),
             ],
