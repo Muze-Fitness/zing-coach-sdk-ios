7b9311a734b1e05765924872c5be5d1d5496371e
Mikhail Vospennikov <mikhail.vospennikov@zing.coach>
2026-06-05 17:18:31 +0300

    Version 1.6.0

--- Package.swift
+++ Package.swift
@@ -9,15 +9,15 @@ let package = Package(
         .library(name: "ZingCoach", targets: ["ZingCoach"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Muze-Fitness/zing-coach-ios-tools", exact: "1.2.0"),
+        .package(url: "https://github.com/Muze-Fitness/zing-coach-ios-tools", exact: "1.4.0"),
         .package(url: "https://github.com/airbnb/lottie-ios.git", .upToNextMajor(from: "4.5.1")),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.6.0")),
     ],
     targets: [
         .binaryTarget(
             name: "ZingCoachSDK",
-            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios/releases/download/1.5.0/ZingCoachSDK.xcframework.zip",
-            checksum: "2655f6557a724810169e427dc399f1d92a726447ddffc853f6b784ea655679b3"
+            url: "https://github.com/Muze-Fitness/zing-coach-sdk-ios/releases/download/1.6.0/ZingCoachSDK.xcframework.zip",
+            checksum: "209757b7f1c9b1e1ee48da91b6f412d1b43e064a023bc62d426ce2898d0fc72d"
         ),
         .target(
             name: "ZingCoach",
